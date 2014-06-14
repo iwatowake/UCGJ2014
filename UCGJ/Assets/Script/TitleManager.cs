a15e71e4ecb9f2e183ce32c5e1040ecb7f39dd70
okonomi <oknm@okonomi.sakura.ne.jp>
2014-06-15 07:25:11 +0900

    タイトル画面追加

---
+++ UCGJ/Assets/Script/TitleManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.type == EventType.KeyDown)
+        {
+            Application.LoadLevel("TestMain");
+        }
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+}
