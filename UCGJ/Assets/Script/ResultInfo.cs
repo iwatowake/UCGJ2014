1afce1505a545d6cd88e7d808fd59a925687d0f8
ShiroKuroShiro <tyousenasagao@hotmail.com>
2014-06-15 08:14:21 +0900

    ResultInfo

---
+++ UCGJ/Assets/Script/ResultInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultInfo : SingletonMonoBehaviour<ResultInfo> {
+
+	public class ResultDate{
+		public int playerNum;
+		public Sprite sp;
+		public int score;
+	}
+
+	public ResultData[] data;
+}
