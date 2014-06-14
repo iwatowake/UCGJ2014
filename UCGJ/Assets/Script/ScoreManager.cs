d140a211648307900ea954a9605ae43d4a7e0746
okonomi <oknm@okonomi.sakura.ne.jp>
2014-06-14 19:01:07 +0900

    名前がかぶらないように変更

--- UCGJ/Assets/Script/ScoreManager.cs
+++ UCGJ/Assets/Script/ScoreManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Manager : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
     // Use this for initialization
     void Start ()
