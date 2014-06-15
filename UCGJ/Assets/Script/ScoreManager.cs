b4d060091329bb398cd41a0ed6eafb4063c02eea
okonomi <oknm@okonomi.sakura.ne.jp>
2014-06-15 09:04:03 +0900

    スコアを取り出して表示

--- UCGJ/Assets/Script/ScoreManager.cs
+++ UCGJ/Assets/Script/ScoreManager.cs
@@ -4,16 +4,31 @@ using System.Collections;
 public class ScoreManager : MonoBehaviour
 {
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        PlayerResult[] objs = GameObject.FindObjectsOfType<PlayerResult> ();
-        for (int i = 0; i < objs.Length; i++) {
-            objs [i].scoreGUIText.text = "" + i;
+        var resultInfo = ResultInfo.Instance;
+
+
+        // dummy data
+        for (int i = 0; i < 4; i++)
+        {
+            var resultData = new ResultInfo.ResultData();
+            resultData.playerNum = i;
+            resultData.score = i * 100;
+            resultInfo.data.Add(resultData);
+        }
+
+
+        var resultDataList = resultInfo.data.ToArray();
+        PlayerResult[] objs = GameObject.FindObjectsOfType<PlayerResult>();
+        for (int i = 0; i < objs.Length; i++)
+        {
+            objs[i].scoreGUIText.text = "" + resultDataList[i].score;
         }
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
 
     }
