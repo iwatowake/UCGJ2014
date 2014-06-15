369116a260b2f636adcd37784bf25191b98a5795
ShiroKuroShiro <tyousenasagao@hotmail.com>
2014-06-15 14:09:52 +0900

    らいせんす

---
+++ UCGJ/Assets/Script/LicenceManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LicenceManager : MonoBehaviour {
+	IEnumerator Start () {
+		yield return new WaitForSeconds(3.0f);
+		Application.LoadLevel("Title");
+	}
+}
